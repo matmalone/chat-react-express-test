35c7853185275063fbd60e1102e72fe563c76d65
Mat Malone <matmalone@gmail.com>
2017-10-01 16:02:15 -0700

    server on port 3001

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = 3001;
 
 var bodyParser = require('body-parser')
 // app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -35,8 +36,8 @@ app.post('/send', (req, res) => {
   res.send(JSON.stringify(status));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port)
 });
 
 
